f205501cbc4b76ae5fa704af1f059373f1c4fbdd
Ahad <105060305+AhadBAzad@users.noreply.github.com>
2023-06-30 19:24:12 +0000

    changes in about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a social junkie from Bangladesh. I have always had a thing for knowing new people and a good relationship with technology only helps my purpose.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
